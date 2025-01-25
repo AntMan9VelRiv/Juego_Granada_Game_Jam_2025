bcb5bacc17f3c76a776df381761ed9145def14c0
AntMan9VelRiv <antonioman987@gmail.com>
2025-01-25 14:26:38 +0100

    Animaciones y sprite de habilidad hechos2

--- toxicbubble/escenas/habilidad.gd
+++ toxicbubble/escenas/habilidad.gd
@@ -3,6 +3,8 @@ extends Node2D  # Manteniendo Node2D
 var velocidad = Vector2()  # Inicializa la velocidad
 var velocidad_base = 500  # La velocidad base con la que se mueve
 
+var animacion_actual = null
+
 # Referencia al Area2D
 @onready var area = $Area2D  # Asegúrate de que el Area2D sea hijo de este nodo
 
@@ -12,6 +14,8 @@ func _ready():
 	
 	# Asignamos la dirección de la habilidad
 	velocidad.x = velocidad_base  # A la derecha por defecto
+	cambiar_animacion("habilidad_derecha")
+	
 	# O si es hacia la izquierda, usamos un valor negativo
 	if scale.x == -1:
 		velocidad.x = -velocidad_base
@@ -23,3 +27,8 @@ func _process(delta):
 func _on_Habilidad_body_entered(body: Node) -> void:
 	# Acciones cuando la habilidad colisiona con algo
 	queue_free()  # Destruir la habilidad al colisionar
+	
+func cambiar_animacion(nueva_animacion):
+	if nueva_animacion != animacion_actual:
+		$Sprite2D/AnimationPlayer.play(nueva_animacion)
+		animacion_actual = nueva_animacion

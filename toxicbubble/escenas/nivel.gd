d1d93c7aa00bc690a2f3664eb2b54533f29487ca
AntMan9VelRiv <antonioman987@gmail.com>
2025-01-26 13:41:24 +0100

    Mapa terminado

--- toxicbubble/escenas/nivel.gd
+++ toxicbubble/escenas/nivel.gd
@@ -13,7 +13,7 @@ func _ready():
 		Controlador.asignar_personaje(personaje)
 		Controlador.asignar_label_vidas(label_vidas)
 		Controlador.asignar_label_burbujas(label_burbujas)
-		Controlador.total_burbujas = 1	
+		Controlador.total_burbujas = 10
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body == personaje:
